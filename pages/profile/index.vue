61eacc9de789255a4606653fe9a4d3af471a5104
Oniel <51058739+0niel@users.noreply.github.com>
2021-09-25 14:25:59 +0300

    Create profile event cards

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -6,7 +6,7 @@
           <b-col> <ProfileInfoCard /></b-col>
           <b-col cols="8">
             <div class="profile-block" style="padding: 10px; display: grid">
-              <div style="width: 100%; text-align: right">
+              <div class="title-with-link">
                 <span class="block-title">Мои достижения</span>
                 <span class="block-title-link">Посмотерть все (23)</span>
               </div>
@@ -46,7 +46,7 @@
                   <progress
                     value="10"
                     max="100"
-                    style="margin-right: 10px; width: 75%"
+                    id="goal-progressbar"
                   ></progress>
                   <span id="progress-text">10% пройдено</span>
                 </div>
@@ -93,8 +93,115 @@
                 </div>
                 <button id="goal-continue-button">Продолжить</button>
               </div>
-            </div></b-col
-          >
+            </div>
+            <div class="profile-block">
+              <div style="padding-bottom: 20px; display: grid">
+                <h2 class="block-title" style="padding: 10px">
+                  Персональные рекомендации
+                </h2>
+                <div
+                  class="title-with-link"
+                  style="padding-left: 10px; padding-right: 10px"
+                >
+                  <span class="block-subtitle">Обучение</span>
+                  <span class="block-title-link">Посмотерть все (23)</span>
+                </div>
+              </div>
+              <img
+                class="image-block"
+                src="https://via.placeholder.com/502x290"
+              />
+              <div class="event-info">
+                <svg
+                  width="30"
+                  height="30"
+                  viewBox="0 0 30 30"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M15 22.5C19.1421 22.5 22.5 19.1421 22.5 15C22.5 10.8579 19.1421 7.5 15 7.5C10.8579 7.5 7.5 10.8579 7.5 15C7.5 19.1421 10.8579 22.5 15 22.5Z"
+                    stroke="#575D5E"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M15 10.5V15L18 16.5"
+                    stroke="#575D5E"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  /></svg
+                ><span class="event-time-text">23 марта 2021</span>
+                <p class="event-title">
+                  Курсы по Figma от Александра Наташкина
+                </p>
+                <button class="event-button">Хочу участвовать</button>
+              </div>
+            </div>
+
+            <div class="profile-block">
+              <div style="padding-bottom: 20px; display: grid">
+                <div
+                  class="title-with-link"
+                  style="padding-left: 10px; padding-right: 10px"
+                >
+                  <span class="block-subtitle">Мероприятия</span>
+                  <span class="block-title-link">Посмотерть все (23)</span>
+                </div>
+              </div>
+              <img
+                class="image-block"
+                src="https://via.placeholder.com/502x290"
+              />
+              <div class="event-info">
+                <svg
+                  width="30"
+                  height="30"
+                  viewBox="0 0 30 30"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M15 22.5C19.1421 22.5 22.5 19.1421 22.5 15C22.5 10.8579 19.1421 7.5 15 7.5C10.8579 7.5 7.5 10.8579 7.5 15C7.5 19.1421 10.8579 22.5 15 22.5Z"
+                    stroke="#575D5E"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M15 10.5V15L18 16.5"
+                    stroke="#575D5E"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  /></svg
+                ><span class="event-time-text">23 марта 2021</span>
+                <p class="event-title">Выставка «Body Worlds. Мир тела»</p>
+                <svg
+                  width="30"
+                  height="30"
+                  viewBox="0 0 30 30"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M21.75 13.5C21.75 18.75 15 23.25 15 23.25C15 23.25 8.25 18.75 8.25 13.5C8.25 11.7098 8.96116 9.9929 10.227 8.72703C11.4929 7.46116 13.2098 6.75 15 6.75C16.7902 6.75 18.5071 7.46116 19.773 8.72703C21.0388 9.9929 21.75 11.7098 21.75 13.5Z"
+                    stroke="#575D5E"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M15 15.75C16.2426 15.75 17.25 14.7426 17.25 13.5C17.25 12.2574 16.2426 11.25 15 11.25C13.7574 11.25 12.75 12.2574 12.75 13.5C12.75 14.7426 13.7574 15.75 15 15.75Z"
+                    stroke="#575D5E"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  /></svg
+                ><span class="event-time-text"
+                  >г. Москва, ВДНХ, павильон 21</span
+                >
+
+                <button class="event-button">Хочу участвовать</button>
+              </div>
+            </div>
+          </b-col>
         </b-row>
       </b-container>
     </PageBase>
@@ -107,6 +214,67 @@ export default {};
 
 <style lang="scss">
 @import "../../assets/constants.scss";
+
+.event-button {
+  font-size: 18px;
+  width: 100%;
+  margin-top: 20px;
+  align-items: center;
+
+  color: $font-bright-color;
+
+  height: 50px;
+
+  padding: 0;
+  border: none;
+
+  background: $accent-color-red;
+  border-radius: 10px;
+}
+
+.event-title {
+  margin: 0;
+  padding-left: 5px;
+  padding-top: 3px;
+  font-family: Inter;
+  font-weight: bold;
+  font-size: 18px;
+
+  color: #2d3436;
+}
+
+.event-time-text {
+  position: relative;
+  top: 1px;
+  font-family: Inter;
+
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 32px;
+
+  color: rgba(45, 52, 54, 0.8);
+}
+
+.event-info {
+  padding: 14px 26px 14px 26px;
+}
+
+.image-block {
+  width: 100%;
+  height: 290px;
+  border-radius: 20px 20px 0px 0px;
+}
+
+.title-with-link {
+  padding-top: 10px;
+  width: 100%;
+  text-align: right;
+}
+
+#goal-progressbar {
+  margin-right: 10px;
+  width: 75%;
+}
 
 #goal-continue-button {
   font-size: 18px;
@@ -201,6 +369,15 @@ progress::-webkit-progress-bar {
   font-size: 18px;
 
   color: #ff4459;
+}
+
+.block-subtitle {
+  float: left;
+  font-family: Inter;
+  font-weight: bold;
+  font-size: 18px;
+
+  color: #2d3436;
 }
 
 .block-title {
