fb44ad7f85e1a868912834421e08c3611785ec80
Oniel <51058739+0niel@users.noreply.github.com>
2021-09-25 18:52:36 +0300

    Update profile page

--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <PageBase>
-      <b-container class="bv-example-row">
-        <b-row>
-          <b-col> <ProfileInfoCard /></b-col>
-          <b-col cols="8">
-            <AchievementsCard />
-            <GoalsCard />
-            <EventCard />
-          </b-col>
-        </b-row>
-      </b-container>
+      <div class="content">
+        <b-container class="bv-example-row">
+          <b-row>
+            <b-col> <ProfileInfoCard /></b-col>
+            <b-col cols="8">
+              <AchievementsCard />
+              <GoalsCard />
+              <RecomendsCard />
+              <EventCard />
+            </b-col>
+          </b-row>
+        </b-container>
+      </div>
     </PageBase>
   </div>
 </template>
@@ -19,13 +22,16 @@
 export default {
   components: {
     AchievementsCard: () => {
-      return import("~/components/profile/AchievementsCard.vue"); //указать нужный путь
+      return import("~/components/profile/AchievementsCard.vue");
     },
     GoalsCard: () => {
-      return import("~/components/profile/GoalsCard.vue"); //указать нужный путь
+      return import("~/components/profile/GoalsCard.vue");
     },
     EventCard: () => {
-      return import("~/components/profile/EventCard.vue"); //указать нужный путь
+      return import("~/components/profile/EventCard.vue");
+    },
+    RecomendsCard: () => {
+      return import("~/components/profile/RecomendsCard.vue");
     },
   },
 };
@@ -33,4 +39,8 @@ export default {
 
 <style lang="scss">
 @import "../../assets/constants.scss";
+
+.content {
+  padding-top: 40px;
+}
 </style>
